969942d5f9f5f23258ff24c2aa16a8f93356da78
oreomaker <zh002919@outlook.com>
2023-10-17 21:20:43 +0800

    feat: first card

---
+++ src/views/instructor/components/base-info.vue
@@ -0,0 +1,28 @@
+<template>
+  <a-descriptions :data="data" title="User Info" bordered />
+</template>
+
+<script lang="ts" setup>
+  const data = [
+    {
+      label: 'Name',
+      value: 'Socrates',
+    },
+    {
+      label: 'Mobile',
+      value: '123-1234-1234',
+    },
+    {
+      label: 'Residence',
+      value: 'Beijing',
+    },
+    {
+      label: 'Hometown',
+      value: 'Beijing',
+    },
+    {
+      label: 'Address',
+      value: 'Yingdu Building, Zhichun Road, Beijing',
+    },
+  ];
+</script>
--- src/views/instructor/index.vue
+++ src/views/instructor/index.vue
@@ -1,1 +1,21 @@
-<template> empty </template>
+<template>
+  <div class="container">
+    <a-card class="general-card">
+      <BaseInfo />
+    </a-card>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import BaseInfo from './components/base-info.vue';
+</script>
+
+<style scoped lang="less">
+  .container {
+    padding: 20px;
+  }
+
+  .general-card {
+    padding-top: 10px;
+  }
+</style>
