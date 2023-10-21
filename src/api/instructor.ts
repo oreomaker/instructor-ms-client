b6caef5cebbe0e74b6a5911606d9408adfad03fa
oreomaker <zh002919@outlook.com>
2023-10-21 13:05:25 +0800

    refactor: rename base info

--- src/api/instructor.ts
+++ src/api/instructor.ts
@@ -1,17 +1,27 @@
+import axios from 'axios';
+
 export interface BaseInfo {
   name: string;
-  race: string;
-  name_pinyin: string;
-  used_name: string;
-  marriage: string;
-  birthday: string;
-  country: string;
-  birthplace: string;
-  political_status: string;
-  id_number: string;
-  high_school_loc: string;
-  religion: string;
+  ethinicity: string; // 民族
+  pinyin_name: string;
+  former_name: string;
+  birth_date: string;
+  marriage_status: string;
+  nationality: string; // 国籍
+  hometown: string; // 籍贯
+  political_affiliation: string; // 政治面貌
+  id_type: string; // 证件类型
+  id_number: string; // 证件号码
+  origin_place: string; // 生源地
+  religious_belief: string; // 宗教信仰
   height: string;
-  health: string;
+  health_status: string;
   weight: string;
+}
+
+export function getBaseInfo() {
+  return axios.get<BaseInfo>('/api/base-info');
+}
+export function updateBaseInfo(data: BaseInfo) {
+  return axios.post<BaseInfo>('/api/base-info', data);
 }
